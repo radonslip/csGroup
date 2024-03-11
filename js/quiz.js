be182dea906ad85ca1dcfb7fab5caf0699f4e71a
Angerydoge <jarvisjameshutchinson@gmail.com>
2024-03-11 14:37:59 +0000

    Set up phaser for responsive development

--- js/quiz.js
+++ js/quiz.js
@@ -1,14 +1,25 @@
-class quiz extends Phaser.Scene
+class quizScene extends Phaser.Scene
 {
     constructor() {
         super('quiz');
         
     }
 
+    preload()
+    {
+
+    }
+
     create()
     {
-        let text = this.add.text(400,400,"hello");
-        text.setScale(3);
+        let text = this.add.text(400,300,"hello").setOrigin(0.5,0.5).setResolution(10);
+        text.setStyle({
+            fontSize: '64px',
+            fontFamily: 'Arial, Helvetica, sans-serif',
+            color: '#000000',
+            align: 'center',
+        })
+
     }
 }
 
@@ -18,12 +29,20 @@ class quiz extends Phaser.Scene
 
 let config = 
 {
-    width: 720,
-    height: 1360,
-    pixelArt: true,
+    type: Phaser.AUTO,
+    input: { mouse: { preventDefaultWheel: false } },
+    debug: true,
     backgroundColor: "#678a7a",
-    autoCenter: true,
-    scene: [quiz]
+    pixelart: true,
+    parent: quizScene,
+    scale: {
+        mode: Phaser.Scale.RESIZE, //Using the RESIZE Scale mode means that CSS can be used to define how to game will be viewed
+        parent: 'quizDiv', //Make Phaser Viewport fit into HTML div
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 800,
+        height: 600
+    },
+    scene: [quizScene]
 }
 
 let conf = new Phaser.Game(config);
